f7780f0f5d18cd75285edddd529b4ce3fe26630c
Peter McIntyre <peter.mcintyre@gmail.com>
2018-02-03 19:46:53 +1100

    problem 3 solved

---
+++ problem3/main.go
@@ -0,0 +1,47 @@
+// Package problem3 The prime factors of 13195 are 5, 7, 13 and 29.
+// What is the largest prime factor of the number 600851475143 ?
+
+package problem3
+
+import (
+	"math"
+)
+
+// Run run
+func Run(input int) int {
+	factors := PrimeFactors(input)
+
+	max := 0
+	for _, e := range factors {
+		if e > max {
+			max = e
+		}
+	}
+
+	return max
+}
+
+// PrimeFactors returns sequence of prime factors of a given integer
+// https://en.wikipedia.org/wiki/Integer_factorization
+// https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
+func PrimeFactors(n int) []int {
+
+	save := []int{}
+	for n%2 == 0 {
+		save = append(save, 2)
+		n /= 2
+	}
+
+	for i := 3; float64(i) <= math.Sqrt(float64(n)); i += 2 {
+		for n%i == 0 {
+			save = append(save, i)
+			n /= i
+		}
+	}
+
+	if n > 2 {
+		save = append(save, n)
+	}
+
+	return save
+}
---
+++ problem3/main_test.go
@@ -0,0 +1,40 @@
+package problem3
+
+import (
+	"reflect"
+	"testing"
+)
+
+var examples = []struct {
+	input   int
+	factors []int
+	answer  int
+}{
+	{100, []int{2, 2, 5, 5}, 5},
+	{144, []int{2, 2, 2, 2, 3, 3}, 3},
+	{315, []int{3, 3, 5, 7}, 7},
+	{360, []int{2, 2, 2, 3, 3, 5}, 5},
+	{8051, []int{83, 97}, 97},
+	{13195, []int{5, 7, 13, 29}, 29},
+	{600851475143, []int{71, 839, 1471, 6857}, 6857},
+}
+
+func TestPrimeFactors(t *testing.T) {
+
+	for _, ex := range examples {
+		result := PrimeFactors(ex.input)
+		if !reflect.DeepEqual(ex.factors, result) {
+			t.Errorf("\nresult: %+v\nexpected: %d\ninput: %+v", result, ex.answer, ex.input)
+		}
+	}
+}
+
+func TestRun(t *testing.T) {
+
+	for _, ex := range examples {
+		result := Run(ex.input)
+		if !reflect.DeepEqual(ex.answer, result) {
+			t.Errorf("\nresult: %+v\nexpected: %d\ninput: %+v", result, ex.answer, ex.input)
+		}
+	}
+}
