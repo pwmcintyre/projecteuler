0c3c7c9f9aadc7e19c91f561065482ff9e7fccb3
Peter McIntyre <peter.mcintyre@gmail.com>
2018-02-03 16:22:05 +1100

    solved problem 1

---
+++ problem1/1.go
@@ -0,0 +1,23 @@
+// Package problem1 Run If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+package problem1
+
+// Input input
+type Input struct {
+	below, multa, multb int
+}
+
+// Run run
+func Run(in Input) int {
+
+	accumulator := 0
+
+	for i := 0; i < in.below; i++ {
+		if i%in.multa == 0 || i%in.multb == 0 {
+			accumulator += i
+		}
+	}
+
+	return accumulator
+}
---
+++ problem1/1_test.go
@@ -0,0 +1,28 @@
+package problem1
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Example struct {
+	input  Input
+	answer int
+}
+
+func TestProblem1(t *testing.T) {
+
+	examples := []Example{
+		{Input{10, 3, 5}, 23},
+		{Input{1000, 3, 5}, 23},
+	}
+
+	fmt.Print(examples)
+
+	for _, ex := range examples {
+		result := Run(ex.input)
+		if result != ex.answer {
+			t.Errorf("\nresult: %+v\nexpected: %d\ninput: %+v", result, ex.answer, ex.input)
+		}
+	}
+}
