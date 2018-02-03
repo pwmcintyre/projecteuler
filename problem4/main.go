f51dc9251074bd7d6764c0dd535fb3f2bc6bd5a2
Peter McIntyre <peter.mcintyre@gmail.com>
2018-02-03 20:38:42 +1100

    problem 4 solved

---
+++ problem4/main.go
@@ -0,0 +1,40 @@
+// Package problem4 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+package problem4
+
+import (
+	"errors"
+	"math"
+
+	"github.com/pwmcintyre/projecteuler/functions"
+)
+
+// Run run
+func Run(digits int) (int, error) {
+	results, err := PalindromicNumbers(digits)
+	return functions.MaxPositiveInt(results), err
+}
+
+func PalindromicNumbers(digits int) ([]int, error) {
+
+	largest := int(math.Pow(10, float64(digits)) - 1)
+	smallest := int(math.Pow(10, float64(digits-1)))
+
+	results := []int{}
+
+	for a := largest; a > smallest; a-- {
+		for b := a - 1; b > smallest; b-- {
+			result := a * b
+			if functions.IsPalindromic(result) {
+				results = append(results, result)
+			}
+		}
+	}
+
+	if len(results) == 0 {
+		return nil, errors.New("no palindromic numbers found")
+	}
+
+	return results, nil
+}
---
+++ problem4/main_test.go
@@ -0,0 +1,30 @@
+package problem4
+
+import (
+	"reflect"
+	"testing"
+)
+
+var examples = []struct {
+	input   int
+	answer  int
+	numbers []int
+}{
+	{1, 8, []int{4, 2}},
+	{2, 9009, []int{91, 99}},
+	{3, 906609, []int{91, 99}},
+	// {4, 99000099, []int{91, 99}}, // too slow!
+}
+
+func TestRun(t *testing.T) {
+
+	for _, ex := range examples {
+		result, err := Run(ex.input)
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(ex.answer, result) {
+			t.Errorf("\nresult: %+v\nexpected: %d\ninput: %+v", result, ex.answer, ex.input)
+		}
+	}
+}
