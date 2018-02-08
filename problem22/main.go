a1e0d391580e4d733e9d2d9393526ff99bb1468d
Peter McIntyre <peter.mcintyre@versent.com.au>
2018-02-08 22:04:47 +1100

    problem 22 solved

--- problem22/main.go
+++ problem22/main.go
@@ -12,14 +12,59 @@ What is the total of all the name scores in the file?
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"sort"
+	"strings"
 )
 
 // Run run
 func Run(input string) (answer int) {
 
-	fmt.Println(input)
+	names := Parse(input)
+	Sort(names)
+
+	for i, name := range names {
+		score := 0
+		for i := range name {
+			s, err := LetterScore(name[i])
+			if err != nil {
+				panic("found non-letter " + name)
+			}
+			score += s
+		}
+		score *= i + 1
+		answer += score
+	}
+
+	return
+}
+
+const UPPER_MIN = 65
+const LOWER_MIN = 97
+
+func LetterScore(letter byte) (int, error) {
+	if letter >= UPPER_MIN && letter < UPPER_MIN+26 {
+		return int(letter - UPPER_MIN + 1), nil
+	} else if letter >= LOWER_MIN && letter < LOWER_MIN+26 {
+		return int(letter - LOWER_MIN + 1), nil
+	}
+	return 0, errors.New("not a letter: " + string(letter))
+}
+
+func Sort(slice []string) {
+	sort.Strings(slice)
+}
+
+func Parse(input string) (names []string) {
+
+	// clean
+	input = strings.Replace(input, `"`, "", -1)
+	input = strings.Replace(input, ` `, "", -1)
+
+	// split
+	names = strings.Split(input, ",")
 
 	return
 }
--- problem22/main_test.go
+++ problem22/main_test.go
@@ -1,21 +1,74 @@
 package main
 
 import (
+	"io/ioutil"
 	"reflect"
 	"testing"
 )
 
-var examples = []struct {
-	input  string
-	answer int
-}{
-	{`"MARY", "PATRICIA", "LINDA"`, 0},
-}
-
 func TestRun(t *testing.T) {
+
+	var examples = []struct {
+		input  string
+		answer int
+	}{
+		{`"ABC"`, 1 * (1 + 2 + 3)},
+		{`"Z", "ABC"`, 2*26 + 1*(1+2+3)},
+	}
+
 	for _, ex := range examples {
 		result := Run(ex.input)
 		if !reflect.DeepEqual(ex.answer, result) {
+			t.Errorf("\nresult: %+v\nexpected: %d\ninput: %+v", result, ex.answer, ex.input)
+		}
+	}
+}
+
+// COLIN ... is the 938th name in the list
+func TestParseSort(t *testing.T) {
+
+	dat, err := ioutil.ReadFile("./names.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	names := Parse(string(dat))
+	if names[0] != "MARY" {
+		t.Error("error parsing")
+	}
+
+	Sort(names)
+	if names[937] != "COLIN" {
+		t.Error("error sorting: " + names[937])
+	}
+
+}
+
+func TestLetterScore(t *testing.T) {
+
+	var examples = []struct {
+		input  string
+		answer int
+		err    bool
+	}{
+		{"@", 0, true},
+		{"!", 0, true},
+		{"a", 1, false},
+		{"A", 1, false},
+		{"z", 26, false},
+		{"Z", 26, false},
+		{"C", 3, false},
+		{"O", 15, false},
+		{"L", 12, false},
+		{"I", 9, false},
+		{"N", 14, false},
+	}
+
+	for _, ex := range examples {
+		result, err := LetterScore(ex.input[0])
+		if ex.err && err == nil {
+			t.Errorf("should be an error: %+v", ex.input)
+		} else if !reflect.DeepEqual(ex.answer, result) {
 			t.Errorf("\nresult: %+v\nexpected: %d\ninput: %+v", result, ex.answer, ex.input)
 		}
 	}
