a596e33cf9130215628e462a845ae36eb03d0d3b
Peter McIntyre <peter.mcintyre@gmail.com>
2019-01-05 17:22:23 +1100

    add 29

---
+++ problems/p0029/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+
+	var examples = []struct {
+		input  input
+		answer answer
+	}{
+		{input{2, 5, 2, 5}, 15},
+		// {input{2, 100, 2, 100}, 997}, // not 997
+	}
+
+	for _, ex := range examples {
+		result := Run(ex.input)
+		if !reflect.DeepEqual(ex.answer, result) {
+			t.Errorf("\nresult: %+v\nexpected: %d\ninput: %+v", result, ex.answer, ex.input)
+		}
+	}
+}
